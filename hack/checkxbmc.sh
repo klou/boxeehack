4397f949d9acf61514a23037b05549b722966ff6
klou <kris.lou@gmail.com>
2013-11-11 01:12:05 -0800

    stripping off the newline being added

--- hack/checkxbmc.sh
+++ hack/checkxbmc.sh
@@ -1,6 +1,9 @@
-echo '0' > /data/etc/.xbmc_found
+# Note: To follow conventions of other flags in /data/etc, this is "reverse boolean".
+# 0 = false
+# 1 = true
+echo -n '0' > /data/etc/.xbmc_found
 for m in /tmp/mnt/*; do
 	if [ -f ${m}/xbmc.bin ] || [ -f ${m}/xbmc/xbmc.bin ]; then
-		echo '1' > /data/etc/.xbmc_found
+		echo -n '1' > /data/etc/.xbmc_found
 	fi
 done
